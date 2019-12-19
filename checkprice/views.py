eea43b1f8aa2611c6ef1792d669e275742da16b2
Flywolfs <zhangc9@student.unimelb.edu.au>
2019-12-20 00:21:02 +0800

    add dynamic show item info

--- checkprice/views.py
+++ checkprice/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,JsonResponse
 # Create your views here.
 
 def search_item(request):
 
-    if request.method == 'POST' :
-        pass
-    ctx={"name":"","cn_official_price":"","hk_official_price":"","other_people_price":"","our_price":"","pic":""}
-    return render(request,"search.html",{"data":ctx})
+    if request.method == 'POST' and len(request.POST) == 1 and "item_name" in request.POST:
+        print("enter search item!"+request.POST.get("item_name"))
+        ctx = {"name": "xxx", "cn_official_price": "xxx", "hk_official_price": "xxx", "other_people_price": "xxx", "our_price": "xxx",
+               "pic": "xxx"}
+        return JsonResponse(ctx)
+    elif request.method == 'GET':
+        return render(request,"search.html")
 
 def record_item(request):
     pass
